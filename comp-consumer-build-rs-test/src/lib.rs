a0f6ea911f27c3d6102e54b955d46d8c7976a5f5
itowlson <ivan.towlson@fermyon.com>
2026-01-23 11:58:05 +1300

    dep generator thing

--- comp-consumer-build-rs-test/src/lib.rs
+++ comp-consumer-build-rs-test/src/lib.rs
@@ -1,17 +1,24 @@
 use spin_sdk::http::{IntoResponse, Request, Response};
 use spin_sdk::http_component;
 
-use macka;
+// use macka;
 
-macka::imports_for!("comp-consumer-build-rs-test");
+// macka::imports_for!("comp-consumer-build-rs-test");
+
+spin_sdk::wit_bindgen::generate!({
+    path: "./spin-dependencies.wit",
+    world: "root",
+    runtime_path: "::spin_sdk::wit_bindgen::rt",
+    generate_all,
+});
 
 #[http_component]
 fn handle_comp_consumer_build_rs_test(_req: Request) -> anyhow::Result<impl IntoResponse> {
-    useless_dep::useless::feature::the_useless_thing::do_the_useless_thing();
-    useless_dep::useless::feature::argh::do_argh();
+    useless::feature::the_useless_thing::do_the_useless_thing();
+    useless::feature::argh::do_argh();
 
-    useful_dep::useful::thingy::this_is_terribly_exciting::thrill_me();
-    println!("1 + 1 = {}", useful_dep::useful::thingy::this_is_also_terribly_exciting::surprise_me(1));
+    useful::thingy::this_is_terribly_exciting::thrill_me();
+    println!("1 + 1 = {}", useful::thingy::this_is_also_terribly_exciting::surprise_me(1));
     
     Ok(Response::builder()
         .status(200)
